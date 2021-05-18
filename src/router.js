768ddc04bc8138663bc281d39c9f6ad272dbc5ac
almishevnurbol <72563743+almishevnurbol@users.noreply.github.com>
2021-05-18 23:39:29 +0600

    Text
    Text

---
+++ src/router.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home'
+import store from './store'
+
+
+Vue.use(Router)
+
+
+
+
+const router = new Router ({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Home,
+            beforeEnter: (to, from, next) => {
+                if (store.state.isLogged) next()
+                else {
+                    to.path !== '/register'
+                }
+            }
+        },
+        {
+            path: '/register',
+            component: () => import('./views/Register.vue')
+        },
+        {
+            path: '/profile',
+            component: () => import('./views/Profile.vue'),
+            beforeEnter: (to, from, next) => {
+                if (store.state.isLogged) {
+                    next()
+                    console.log(store.state.isLogged)
+                }
+                else {
+                    console.log(store.state.isLogged)
+                    to.path !== '/register'
+                }
+            }
+        }
+    ]
+})
+
+
+
+
+  
+// router.beforeEach((to, from, next) => {
+//     let isLogged = localStorage.getItem("isLogged")
+//     // console.log(isLogged)
+//     if (  !isLogged && to.path !== '/register' ) {
+//       next("/register")
+//     } else {
+//       next()
+//     }
+// });
+
+export default router
